2591a2a8be7a6f5c48790f4c2538c22341552bff
david <eorjs2020@gmail.com>
2020-07-17 13:48:36 -0400

    Added Collision boundaries on Debug view

--- src/CollisionManager.cpp
+++ src/CollisionManager.cpp
@@ -71,8 +71,13 @@ bool CollisionManager::AABBCheck(GameObject* object1, GameObject* object2)
 			object2->getRigidBody()->isColliding = true;
 
 			switch (object2->getType()) {
-			case TARGET:
-				std::cout << "Collision with Target!" << std::endl;
+			case OBSTACLE:
+				std::cout << "Collision with OBSTACLE!" << std::endl;
+				SoundManager::Instance().playSound("yay", 0);
+				break;
+			
+			case PLANE:
+				std::cout << "Collision with OBSTACLE!" << std::endl;
 				SoundManager::Instance().playSound("yay", 0);
 				break;
 			default:
